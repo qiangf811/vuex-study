b4805f5ad339c4ad063d352ec6c2ca77a1771e63
fengqiang <fengqiang@iflytek.com>
2017-11-22 21:29:14 +0800

    add all file
    add all file

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="hello">
+  <h1>{{ count }}</h1>
+  <h2 @click="add">click me</h2>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  computed: {
+    count() {
+      return this.$store.state.count
+    }
+  },
+  methods: {
+    add() {
+      this.$store.commit('increment')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
